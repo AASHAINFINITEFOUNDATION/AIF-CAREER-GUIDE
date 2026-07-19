39b013d5138b4dee0972dd1c1c84c31ec692122a
AASHA INFINITE FOUNDATION <meera.aasha2014@gmail.com>
2026-07-19 22:00:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const dropdowns = document.querySelectorAll(".dropdown-btn");
-dropdowns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.nextElementSibling.classList.toggle("show");
-  });
-});
